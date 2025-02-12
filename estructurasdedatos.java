ab4e24ec4c2065673138ae01796730add7a3d071
Carla Gonzalez <carlafernanda006@gmail.com>
2025-02-11 20:43:37 -0500

    nuevos docs

---
+++ estructurasdedatos.java
@@ -0,0 +1,16 @@
+//El presente proyecto lleva a cabo la aplicacion de estructuras de datos en java
+
+public class estructurasdedatos {
+    
+    public static void main(String[] args) {
+        //Arreglos
+        //Declaracion de un arreglo de enteros
+        int[] arregloEnteros = new int[5];
+        //Declaracion de un arreglo de cadenas
+        String[] arregloCadenas = new String[5];
+
+        //Asignacion de valores a los arreglos
+        arregloEnteros[0] = 1;
+        arregloEnteros[1] = 2;
+        
+}
---
+++ practica.java
@@ -0,0 +1,11 @@
+public class practica {
+    protected static final int[] arregloEnteros = new int[5];
+
+    public static void main(String[] args) {
+        //Asignacion de valores a los arreglos
+        arregloEnteros[0] = 1;
+        arregloEnteros[1] = 2;
+
+        System.out.println(null == null);
+    }
+}
