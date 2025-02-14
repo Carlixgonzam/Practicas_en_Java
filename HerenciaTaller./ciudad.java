96a6a941e8771918caa2f0f866a6363c732131d3
Carla Gonzalez <carlafernanda006@gmail.com>
2025-02-13 19:18:39 -0500

    practicaherencia

---
+++ HerenciaTaller./ciudad.java
@@ -0,0 +1,14 @@
+package HerenciaTaller;
+
+public class ciudad {
+
+    private String nombre;
+    private void main[] args;
+
+    public ciudad(String nombre, void addTitle(String title), void addConnector(String connector)){
+        this.nombre = nombre();
+        this.addTitle = addtitle();
+        this.addConnector(connector);
+    }
+    
+}
